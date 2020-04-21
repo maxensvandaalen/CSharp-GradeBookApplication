058bdfe0035b6869ae8e33b724c33f759778b4f2
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2020-04-21 15:38:45 +0200

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using GradeBook.Enums;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,5 +12,26 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+
+            var rankedGradeInterval = (int)(Students.Count * 0.2);
+            List<Student> sortedStudents = Students.OrderBy(x => x.AverageGrade).ToList();
+
+            if (averageGrade >= sortedStudents[rankedGradeInterval].AverageGrade)
+                return 'A';
+            else if (averageGrade >= sortedStudents[rankedGradeInterval * 2].AverageGrade)
+                return 'B';
+            else if (averageGrade >= sortedStudents[rankedGradeInterval * 3].AverageGrade)
+                return 'C';
+            else if (averageGrade >= sortedStudents[rankedGradeInterval * 4].AverageGrade)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
