44298fb05b4e35fabddce1d260e4032f09e6defe
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2020-04-21 15:58:44 +0200

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,16 +19,16 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
             }
 
-            var rankedGradeInterval = (int)(Students.Count * 0.2);
-            List<Student> sortedStudents = Students.OrderBy(x => x.AverageGrade).ToList();
+            var rankedGradeInterval = (int)Math.Ceiling(Students.Count * 0.2);
+            List<Student> sortedStudents = Students.OrderByDescending(x => x.AverageGrade).ToList();
 
-            if (averageGrade >= sortedStudents[rankedGradeInterval].AverageGrade)
+            if (averageGrade >= sortedStudents[rankedGradeInterval - 1].AverageGrade)
                 return 'A';
-            else if (averageGrade >= sortedStudents[rankedGradeInterval * 2].AverageGrade)
+            else if (averageGrade >= sortedStudents[(rankedGradeInterval * 2) - 1].AverageGrade)
                 return 'B';
-            else if (averageGrade >= sortedStudents[rankedGradeInterval * 3].AverageGrade)
+            else if (averageGrade >= sortedStudents[(rankedGradeInterval * 3) - 1].AverageGrade)
                 return 'C';
-            else if (averageGrade >= sortedStudents[rankedGradeInterval * 4].AverageGrade)
+            else if (averageGrade >= sortedStudents[(rankedGradeInterval * 4) - 1].AverageGrade)
                 return 'D';
             else
                 return 'F';
